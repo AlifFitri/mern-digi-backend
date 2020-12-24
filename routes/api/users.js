04a54ed2a5ce2d897aacf1c40b13f154d7c7f65a
Alif Fitri <aliffitrics@gmail.com.my>
2020-12-24 13:18:37 +0800

    implement JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 router.post('/', [
@@ -14,15 +16,19 @@ router.post('/', [
     ),
 ],
 async (req, res) => {
+
+  // check for validation error
   const errors = validationResult(req);
   if (!errors.isEmpty()){
     return res.status(400).json({ errors: errors.array()});
   }
   
+  // destructure request body
   const {name, email, password} = req.body;
   
   try {
 
+    // check if user already exists
     let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ errors: [{msg: 'User already exists'}] });
@@ -34,13 +40,29 @@ async (req, res) => {
       password
     });
 
+    // encrypt password
     const salt = await bcrypt.genSalt(10);
-
     user.password = await bcrypt.hash(password, salt);
 
+    // save user data
     await user.save();
 
-    res.send('User registered');
+    const payload = {
+      user: {
+        id: user.id,
+        name: user.name
+      }
+    }
+
+    // sign in for JWT-Token
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
 
 
   } catch(err) {
