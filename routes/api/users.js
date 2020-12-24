0d0d271e2ebb9c389dccae401b1e3b8f42324e6f
Alif Fitri <aliffitrics@gmail.com.my>
2020-12-24 12:49:06 +0800

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/', [
@@ -10,12 +12,43 @@ router.post('/', [
     .matches(
       /^(?=.*[A-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])(?=.*[a-zA-Z]).{12,}$/
     ),
-], (req, res) => {
+],
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()){
     return res.status(400).json({ errors: errors.array()});
   }
-  res.send('User route');
+  
+  const {name, email, password} = req.body;
+  
+  try {
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ errors: [{msg: 'User already exists'}] });
+    }
+
+    user = new User({
+      name,
+      email,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User registered');
+
+
+  } catch(err) {
+
+    console.error(err.message);
+    res.status(500).send('Server error');
+    
+  }
 });
 
 module.exports = router;
